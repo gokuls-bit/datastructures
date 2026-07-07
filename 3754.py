f426c41179f653cb7848fbec74ca64604e51d677
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-08 00:01:38 +0530

    Update 3754.py

--- 3754.py
+++ 3754.py
@@ -1,1 +1,17 @@
+class Solution(object):
+    def sumAndMultiply(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
 
+        x = 0
+        digit_sum = 0
+
+        for ch in str(n):
+            if ch != '0':
+                d = int(ch)
+                x = x * 10 + d
+                digit_sum += d
+
+        return x * digit_sum
