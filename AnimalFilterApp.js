68ccf583df583b053259b588a297d84e405a3f70
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-09 18:38:43 +0530

    Update AnimalFilterApp.js

--- AnimalFilterApp.js
+++ AnimalFilterApp.js
@@ -1,1 +1,49 @@
+import React, { useState } from "react";
+import "./AnimalFilterApp.css";
+
+function AnimalFilterApp() {
+  const [category, setCategory] = useState("Mammals");
+
+  const animals = {
+    Birds: ["Eagle", "Parrot", "Penguin"],
+    Mammals: ["Lion", "Tiger", "Elephant"],
+    Reptiles: ["Snake", "Crocodile", "Lizard"],
+    Fish: ["Shark", "Salmon", "Goldfish"],
+  };
+
+  return (
+    <div className="container">
+
+      <div className="buttons">
+        <button onClick={() => setCategory("Birds")}>
+          Birds
+        </button>
+
+        <button onClick={() => setCategory("Mammals")}>
+          Mammals
+        </button>
+
+        <button onClick={() => setCategory("Reptiles")}>
+          Reptiles
+        </button>
+
+        <button onClick={() => setCategory("Fish")}>
+          Fish
+        </button>
+      </div>
+
+      <div className="cards">
+        {animals[category].map((animal, index) => (
+          <div className="card" key={index}>
+            {animal}
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default AnimalFilterApp;
+
 
