feff3792a7f64e779cd109118cdfc021f7392bf7
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-09 18:17:48 +0530

    Add pathExistenceQueries function
    Implement pathExistenceQueries function to determine if paths exist based on maxDiff.

--- 3532.js
+++ 3532.js
@@ -1,1 +1,29 @@
+/**
+ * @param {number} n
+ * @param {number[]} nums
+ * @param {number} maxDiff
+ * @param {number[][]} queries
+ * @return {boolean[]}
+ */
+var pathExistenceQueries = function(n, nums, maxDiff, queries) {
 
+    const comp = new Array(n);
+    let id = 0;
+
+    comp[0] = 0;
+
+    for (let i = 1; i < n; i++) {
+        if (nums[i] - nums[i - 1] > maxDiff) {
+            id++;
+        }
+        comp[i] = id;
+    }
+
+    const ans = [];
+
+    for (const [u, v] of queries) {
+        ans.push(comp[u] === comp[v]);
+    }
+
+    return ans;
+};
