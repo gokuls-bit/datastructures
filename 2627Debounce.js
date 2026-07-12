2f7fb5f68c16e0b6ddc8687dd49106fef1134417
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-12 15:44:55 +0530

    Update 2627Debounce.js

--- 2627Debounce.js
+++ 2627Debounce.js
@@ -1,1 +1,18 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
 
+    let timer;
+
+    return function(...args) {
+
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+            fn(...args);
+        }, t);
+    };
+};
