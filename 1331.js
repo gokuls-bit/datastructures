d9106376c65c0c4d713d46c7640921a94f14df62
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-12 14:34:36 +0530

    Update 1331.js

--- 1331.js
+++ 1331.js
@@ -1,1 +1,19 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var arrayRankTransform = function(arr) {
 
+    // Create sorted array with unique elements
+    const sorted = [...new Set(arr)].sort((a, b) => a - b);
+
+    // Store rank of each number
+    const rank = new Map();
+
+    for (let i = 0; i < sorted.length; i++) {
+        rank.set(sorted[i], i + 1);
+    }
+
+    // Replace every element with its rank
+    return arr.map(num => rank.get(num));
+};
