c72becc4d02128457bdaba192b9e7eeaebd4ccfb
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-13 23:54:48 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,1 +1,21 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+    return async function(...args) {
+        
+        const timeoutPromise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+        });
 
+        return Promise.race([
+            fn(...args),
+            timeoutPromise
+        ]);
+    }
+};
