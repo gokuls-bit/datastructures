c54fe4b15facc59762905d483e9eefc43c98612b
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-16 00:01:45 +0530

    Update 3658.cpp

--- 3658.cpp
+++ 3658.cpp
@@ -1,1 +1,11 @@
-
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        
+        int oddSum = n * n;
+        
+        int evenSum = n * (n + 1);
+        
+        return gcd(oddSum, evenSum);
+    }
+};
