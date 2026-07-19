59d671d3ae696d707f14b94c887baaf89df175d5
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-19 23:23:30 +0530

    Update 1081.cpp

--- 1081.cpp
+++ 1081.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
 
+        vector<int> lastIndex(26);
+
+        for (int i = 0; i < s.size(); i++) {
+
+            lastIndex[s[i] - 'a'] = i;
+        }
+
+        vector<bool> visited(26, false);
+
+        string result;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            char currentCharacter = s[i];
+
+            if (visited[currentCharacter - 'a']) {
+                continue;
+            }
+
+            while (!result.empty() &&
+                   result.back() > currentCharacter &&
+                   lastIndex[result.back() - 'a'] > i) {
+
+                visited[result.back() - 'a'] = false;
+
+                result.pop_back();
+            }
+
+            result.push_back(currentCharacter);
+
+            visited[currentCharacter - 'a'] = true;
+        }
+
+        return result;
+    }
+};
