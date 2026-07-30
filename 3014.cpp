d7408418be3b6da29bf08a1776f8030df504a5fc
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-07-30 23:03:18 +0530

    Implement minimumPushes function in Solution class

--- 3014.cpp
+++ 3014.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
 
+        int n = word.size();
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            answer += (i / 8) + 1;
+        }
+
+        return answer;
+    }
+};
