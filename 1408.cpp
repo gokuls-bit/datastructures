35e461e48113897bc6cf89ca9fe66792d5b7d39e
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-08-03 13:47:02 +0530

    Update 1408.cpp

--- 1408.cpp
+++ 1408.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
 
+        int n = stoneValue.size();
+
+        vector<int> dp(n + 1, INT_MIN);
+
+        dp[n] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int currentSum = 0;
+
+            for (int take = 0; take < 3 && i + take < n; take++) {
+
+                currentSum += stoneValue[i + take];
+
+                dp[i] = max(
+                    dp[i],
+                    currentSum - dp[i + take + 1]
+                );
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+
+        if (dp[0] < 0)
+            return "Bob";
+
+        return "Tie";
+    }
+};
