245e88e71d3a52bb0c639163c268570bbeb3117d
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-08-20 15:13:41 +0530

    Update 3069.cpp

--- 3069.cpp
+++ 3069.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
 
+        vector<int> first;
+        vector<int> second;
+
+        first.push_back(nums[0]);
+        second.push_back(nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+
+            if (i % 2 == 0) {
+                first.push_back(nums[i]);
+            }
+            else {
+                second.push_back(nums[i]);
+            }
+        }
+
+        for (int x : second) {
+            first.push_back(x);
+        }
+
+        return first;
+    }
+};
