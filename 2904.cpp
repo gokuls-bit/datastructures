17974529bc89b154f18174ff9a10a48bc4911d39
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-08-26 12:08:13 +0530

    Create 2904.cpp

---
+++ 2904.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+
+        int n = s.size();
+
+        int left = 0;
+        int ones = 0;
+
+        string answer = "";
+
+        for (int right = 0; right < n; right++) {
+
+            if (s[right] == '1') {
+                ones++;
+            }
+
+            while (ones > k) {
+
+                if (s[left] == '1') {
+                    ones--;
+                }
+
+                left++;
+            }
+
+            if (ones == k) {
+
+                // Remove unnecessary leading zeros.
+                while (left <= right && s[left] == '0') {
+                    left++;
+                }
+
+                string current =
+                    s.substr(left, right - left + 1);
+
+                if (answer.empty() ||
+                    current.size() < answer.size() ||
+                    (current.size() == answer.size() &&
+                     current < answer)) {
+
+                    answer = current;
+                }
+            }
+        }
+
+        return answer;
+    }
+};
