968541131e653267e737ab3a0b730d8106158f16
GOKUL KUMAR SANT <gokulsant684@gmail.com>
2026-09-02 22:33:53 +0530

    Add Solution class with uniformArray method

---
+++ 3875.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+
+        // It is always possible to construct
+        // an array with uniform parity.
+
+        return true;
+    }
+};
